e881931dd8a1ab9a2415b3c80c57ef99370022e7
Ali Maharramli <tmodpe@gmail.com>
2020-03-14 18:46:00 +0400

    Add solution for problem `A - EhAb AnD gCd` Link: https://codeforces.com/contest/1325/problem/A Tags: constructive algorithms, greedy, number theory, *800 Ref: https://codeforces.com/contest/1325/submission/73233204

---
+++ codeforces/1325/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main(){
+    ll n;
+    cin>>n;
+    for(ll i=0;i<n;i++){
+        ll a;
+        cin>>a;
+        cout<<a-1<<' '<<1<<endl;
+    }
+}
