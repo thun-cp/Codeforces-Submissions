be3e523af8dbeae172af22f30cceb199638efaab
Ali Maharramli <tmodpe@gmail.com>
2020-03-19 19:26:00 +0400

    Add solution for problem `A - Bad Ugly Numbers` Link: https://codeforces.com/contest/1326/problem/A Tags: constructive algorithms, number theory, *1000 Ref: https://codeforces.com/contest/1326/submission/73696197

---
+++ codeforces/1326/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll arr[100005];
+int main(){
+    ll t;
+    cin>>t;
+    while(t>0){
+        ll n;
+        cin>>n;
+        string ma;
+        if(n==1) cout<<-1<<endl;
+        else{
+        cout<<2;
+        for(ll i=0;i<n-1;i++) cout<<3;
+        cout<<endl;
+    }
+    t--;
+    }
+}
