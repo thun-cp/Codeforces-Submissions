a3408a19cac60a0797c9f783616ddb012fb7622a
Ali Maharramli <tmodpe@gmail.com>
2020-06-23 18:41:00 +0400

    Add solution for problem `A - FashionabLee` Link: https://codeforces.com/contest/1369/problem/A Tags: geometry, math, *800 Ref: https://codeforces.com/contest/1369/submission/84780600

---
+++ codeforces/1369/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+void solve(){
+    ll n;
+    cin>>n;
+    if(n%4==0) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--) solve();
+}
