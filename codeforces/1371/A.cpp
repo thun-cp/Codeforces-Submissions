be6d75b9aa608c8ee8c59eabfe6e3c7e501ef5e6
Ali Maharramli <tmodpe@gmail.com>
2020-07-01 17:39:00 +0400

    Add solution for problem `A - Magical Sticks` Link: https://codeforces.com/contest/1371/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1371/submission/85630857

---
+++ codeforces/1371/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+void solve(){
+    ll n;
+    cin>>n;
+    cout<<ll(ceil(n/2.0))<<endl;
+}
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--)
+    solve();
+}
