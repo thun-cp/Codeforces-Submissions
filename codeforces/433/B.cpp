15e9c31c747b7e445ca17cafa77b624b3cb5251d
Ali Maharramli <tmodpe@gmail.com>
2020-07-09 20:33:00 +0400

    Add solution for problem `B - Kuriyama Mirai's Stones` Link: https://codeforces.com/contest/433/problem/B Tags: dp, implementation, sortings, *1200 Ref: https://codeforces.com/contest/433/submission/86368034

---
+++ codeforces/433/B.cpp
@@ -0,0 +1,33 @@
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+void solve(){
+    ll n;
+    cin>>n;
+    vector<ll> arr(n+1,0);
+    for(ll i=1;i<=n;i++) cin>>arr[i];
+    vector<ll> arr1=arr;
+    for(ll i=1;i<=n;i++) arr[i]+=arr[i-1];
+    sort(arr1.begin(),arr1.end());
+    for(ll i=1;i<=n;i++) arr1[i]+=arr1[i-1];
+    ll m;
+    cin>>m;
+    while(m--){
+        ll t,l,r;
+        cin>>t>>l>>r;
+        if(t==1) cout<<arr[r]-arr[l-1]<<endl;
+        else cout<<arr1[r]-arr1[l-1]<<endl;
+    }
+}
+
+int main(){
+    solve();
+}
