7840d59f2b11b3b2ab64c62e8d828613b0912c13
Ali Maharramli <tmodpe@gmail.com>
2020-07-11 23:45:00 +0400

    Add solution for problem `A - Omkar and Completion` Link: https://codeforces.com/contest/1372/problem/A Tags: constructive algorithms, implementation, *800 Ref: https://codeforces.com/contest/1372/submission/86600768

---
+++ codeforces/1372/A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define endl '\n'
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+void solve(){
+    ll n;
+    cin>>n;
+    for(ll i=0;i<n;i++){
+      cout<<1<<' ';
+    }
+    cout<<endl;
+}
+
+int main(){
+    ll t;
+    cin>>t;
+    while(t--)
+    solve();
+}
