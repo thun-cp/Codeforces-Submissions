08765084623f1cc434c723b00cac56756feab9c3
Ali Maharramli <tmodpe@gmail.com>
2020-09-07 19:57:00 +0400

    Add solution for problem `B - Escape` Link: https://codeforces.com/contest/148/problem/B Tags: implementation, math, *1500 Ref: https://codeforces.com/contest/148/submission/92150822

--- codeforces/148/B.cpp
+++ codeforces/148/B.cpp
@@ -1,2 +1,86 @@
-#include <iostream>
-typedef long double ld;typedef long long ll;using namespace std;int main(){ld o_939e7c102ff07e1a305fbc75857746bf,o_445351d28418053f1733f070328c41ab,o_5d3e6342fe262cc95933cd38642535c0,o_07246b912af05f2ef8db8907747b8c35,o_319363f6dcd5343d468a584cbdeca26a;cin>>o_939e7c102ff07e1a305fbc75857746bf>>o_445351d28418053f1733f070328c41ab>>o_5d3e6342fe262cc95933cd38642535c0>>o_07246b912af05f2ef8db8907747b8c35>>o_319363f6dcd5343d468a584cbdeca26a;ld o_009d877f136a6e794a85365f10077116=o_5d3e6342fe262cc95933cd38642535c0 * o_939e7c102ff07e1a305fbc75857746bf;ll o_8cbb3449938b3c24fab867614c9bc4c9=(0x0000000000000000 + 0x0000000000000200 + 0x0000000000000800 - 0x0000000000000A00);ll o_b1e9e9f09494ff69c3e1151c40dc8bc3=(0x0000000000000000 + 0x0000000000000200 + 0x0000000000000800 - 0x0000000000000A00);if ((o_939e7c102ff07e1a305fbc75857746bf >= o_445351d28418053f1733f070328c41ab) & !!(o_939e7c102ff07e1a305fbc75857746bf >= o_445351d28418053f1733f070328c41ab)){cout<<(0x0000000000000000 + 0x0000000000000200 + 0x0000000000000800 - 0x0000000000000A00)<<endl;return (0x0000000000000000 + 0x0000000000000200 + 0x0000000000000800 - 0x0000000000000A00);};while ((o_009d877f136a6e794a85365f10077116 < o_319363f6dcd5343d468a584cbdeca26a) & !!(o_009d877f136a6e794a85365f10077116 < o_319363f6dcd5343d468a584cbdeca26a)){o_009d877f136a6e794a85365f10077116 += o_939e7c102ff07e1a305fbc75857746bf * (o_009d877f136a6e794a85365f10077116 / (o_445351d28418053f1733f070328c41ab - o_939e7c102ff07e1a305fbc75857746bf));if ((o_009d877f136a6e794a85365f10077116 >= o_319363f6dcd5343d468a584cbdeca26a) & !!(o_009d877f136a6e794a85365f10077116 >= o_319363f6dcd5343d468a584cbdeca26a))break;;o_b1e9e9f09494ff69c3e1151c40dc8bc3++;o_009d877f136a6e794a85365f10077116 += o_939e7c102ff07e1a305fbc75857746bf * (o_009d877f136a6e794a85365f10077116 / o_445351d28418053f1733f070328c41ab + o_07246b912af05f2ef8db8907747b8c35);};cout<<o_b1e9e9f09494ff69c3e1151c40dc8bc3<<endl;return (0x0000000000000000 + 0x0000000000000200 + 0x0000000000000800 - 0x0000000000000A00);};
+// #pragma comment(linker, "/stack:200000000")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization("unroll-loops")
+
+#include "bits/stdc++.h"
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define endl '\n'
+#define F first
+#define S second
+#define IO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define cf ll testcases;cin>>testcases;while(testcases--)
+#define prn(x) if(x) cout<<"YES"<<endl; else cout<<"NO"<<endl;
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define minel(x) (*min_element(all(x)))
+#define maxel(x) (*max_element(all(x)))
+#define cou dbg=1;cout
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll,ll> pi;
+typedef vector<ll> vi;
+typedef vector<ld> vd;
+typedef vector<pi> vp;
+ll dbg=0;
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p);
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p);
+template<typename A> istream& operator>>(istream& cin, vector<A> &v);
+int dx[] = {-1, -1, -1,  0, 0,  1, 1, 1};
+int dy[] = {-1,  0,  1, -1, 1, -1, 0, 1};
+
+void solve(){
+    ld vep,ved,t,f,c;
+    cin>>vep>>ved>>t>>f>>c;
+    ld dist=t*vep;
+    ll dist0=0;
+    ll ans=0;
+    if(vep>=ved){
+        cout<<0<<endl;
+        return;
+    }
+    while(dist<c){
+        dist+=vep*(dist/(ved-vep));
+        if(dist>=c) break;
+        ans++;
+        dist+=vep*(dist/ved+f);
+        
+    }
+    cout<<ans<<endl;
+}
+
+int main(){
+    IO;
+    // cf
+    solve();
+    return 0;
+}
+
+
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { 
+    if(dbg){
+        return cout << "(" << p.F << ", " << p.S << ")"; 
+    }else{
+        return cout<<p.F<<' '<<p.S;
+    }
+        dbg=0;}
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
+    if(dbg){
+        cout << "["; for(ll i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+    }else{
+        for(ll i=0;i<v.size()-1;i++) cout<<v[i]<<' ';
+        return cout<<v[v.size()-1];
+    dbg=0;}
+}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
+   cin >> p.first;
+   return cin >> p.second;
+}
+template<typename A> istream& operator>>(istream& cin, vector<A> &v) {
+   for(ll i=0;i<v.size()-1;i++) cin>>v[i];
+   return cin >> v[v.size()-1];
+}
