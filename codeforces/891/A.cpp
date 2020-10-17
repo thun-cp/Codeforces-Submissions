12c554b5395a30b9f4df674853f2cdc6f51b5aed
Ali Maharramli <tmodpe@gmail.com>
2020-10-17 14:59:00 +0400

    Add solution for problem `A - Pride` Link: https://codeforces.com/contest/891/problem/A Tags: brute force, dp, greedy, math, number theory, *1500 Ref: https://codeforces.com/contest/891/submission/95731096

---
+++ codeforces/891/A.cpp
@@ -0,0 +1,104 @@
+// #pragma comment(linker, "/stack:200000000")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization("unroll-loops")
+
+#include "bits/stdc++.h"
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define endl '\n'
+#define F first
+#define S second
+#define IO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define cf ll testcases;cin>>testcases;while(testcases--)
+#define prn(x) if(x) cout<<"YES"<<endl; else cout<<"NO"<<endl;
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define minel(x) (*min_element(all(x)))
+#define maxel(x) (*max_element(all(x)))
+#define cou dbg=1;cout
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll,ll> pi;
+typedef vector<ll> vi;
+typedef vector<ld> vd;
+typedef vector<pi> vp;
+ll dbg=0;
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p);
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p);
+template<typename A> istream& operator>>(istream& cin, vector<A> &v);
+int dx[] = {-1, -1, -1,  0, 0,  1, 1, 1};
+int dy[] = {-1,  0,  1, -1, 1, -1, 0, 1};
+
+void solve(){
+    ll n;
+    cin>>n;
+    vi arr(n);
+    cin>>arr;
+    if(n==1){
+        if(arr[0]==1) cout<<0<<endl;
+        else cout<<-1<<endl;
+        return;
+    }
+    ll alg=__gcd(arr[0],arr[1]);
+    for(ll i=2;i<n;i++) alg=__gcd(alg,arr[i]);
+    if(alg!=1) cout<<-1<<endl;
+    else{
+        ll co=count(all(arr),1);
+        if(co>0) cout<<n-co<<endl;
+        else{
+            ll dist=LLONG_MAX;
+            for(ll i=0;i<n-1;i++){
+                ll num=__gcd(arr[i],arr[i+1]);
+                if(num==1){
+                    dist=1;
+                    break;
+                }
+                for(ll j=i+2;j<n;j++){
+                    if(__gcd(num,arr[j])==1){
+                        dist=min(j-i,dist);
+                    }
+                    num=__gcd(num,arr[j]);
+                }
+            }
+            //cout<<dist<<endl;
+            cout<<n-1+dist<<endl;
+        }
+    }
+    
+}
+
+int main(){
+    IO;
+    // cf
+    solve();
+    return 0;
+}
+
+
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { 
+    if(dbg){
+        return cout << "(" << p.F << ", " << p.S << ")"; 
+    }else{
+        return cout<<p.F<<' '<<p.S;
+    }
+        dbg=0;}
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
+    if(dbg){
+        cout << "["; for(ll i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+    }else{
+        for(ll i=0;i<v.size()-1;i++) cout<<v[i]<<' ';
+        return cout<<v[v.size()-1];
+    dbg=0;}
+}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
+   cin >> p.first;
+   return cin >> p.second;
+}
+template<typename A> istream& operator>>(istream& cin, vector<A> &v) {
+   for(ll i=0;i<v.size()-1;i++) cin>>v[i];
+   return cin >> v[v.size()-1];
+}
