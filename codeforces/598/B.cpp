6e174814c61c4ad3ab8604fa00edcd51b52143e4
Ali Maharramli <tmodpe@gmail.com>
2020-10-26 10:09:00 +0400

    Add solution for problem `B - Queries on a String` Link: https://codeforces.com/contest/598/problem/B Tags: implementation, strings, *1300 Ref: https://codeforces.com/contest/598/submission/96749767

---
+++ codeforces/598/B.cpp
@@ -0,0 +1,94 @@
+// #pragma comment(linker, "/stack:200000000")
+// #pragma GCC optimize("Ofast")
+// #pragma GCC target("avx,avx2,fma")
+// #pragma GCC optimization("unroll-loops")
+
+#include "bits/stdc++.h"
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define endl '\n'
+#define F first
+#define S second
+#define IO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
+#define cf ll testcases;cin>>testcases;while(testcases--)
+#define prn(x) if(x) cout<<"YES"<<endl; else cout<<"NO"<<endl;
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(),x.rend()
+#define minel(x) (*min_element(all(x)))
+#define maxel(x) (*max_element(all(x)))
+#define cou dbg=1;cout
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<ll,ll> pi;
+typedef vector<ll> vi;
+typedef vector<ld> vd;
+typedef vector<pi> vp;
+ll dbg=0;
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p);
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p);
+template<typename A> istream& operator>>(istream& cin, vector<A> &v);
+int dx[] = {-1, -1, -1,  0, 0,  1, 1, 1};
+int dy[] = {-1,  0,  1, -1, 1, -1, 0, 1};
+
+void solve(){
+    string s;
+    cin>>s;
+    ll m;
+    cin>>m;
+    while(m--){
+        ll l,r,k;
+        cin>>l>>r>>k;
+        l--;r--;
+        ll n=r-l+1;
+        k%=n;
+        string str;
+        for(ll i=r;i>r-k;i--){
+            str=s[i]+str;
+        }
+        //cout<<str<<endl;
+        for(ll i=l;i<=r-k;i++){
+            str+=s[i];
+        }
+        for(ll i=l;i<=r;i++){
+            s[i]=str[i-l];
+        }
+        //cout<<str<<endl;
+        //cout<<s<<endl;
+    }
+    cout<<s<<endl;
+}
+
+int main(){
+    IO;
+    // cf
+    solve();
+    return 0;
+}
+
+
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { 
+    if(dbg){dbg=0;
+        return cout << "(" << p.F << ", " << p.S << ")"; 
+    }else{
+        return cout<<p.F<<' '<<p.S;
+    }
+        }
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
+    if(dbg){dbg=0;
+        cout << "["; for(ll i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+    }else{
+        for(ll i=0;i<v.size()-1;i++) cout<<v[i]<<' ';
+        return cout<<v[v.size()-1];
+    }
+}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
+   cin >> p.first;
+   return cin >> p.second;
+}
+template<typename A> istream& operator>>(istream& cin, vector<A> &v) {
+   for(ll i=0;i<v.size()-1;i++) cin>>v[i];
+   return cin >> v[v.size()-1];
+}
