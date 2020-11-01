c94559609d53c7b712f4dcea1ec700b6525556c4
Ali Maharramli <tmodpe@gmail.com>
2020-11-01 11:57:00 +0400

    Add solution for problem `B - Ternary String` Link: https://codeforces.com/contest/1354/problem/B Tags: binary search, dp, implementation, two pointers, *1200 Ref: https://codeforces.com/contest/1354/submission/97290804

--- codeforces/1354/B.cpp
+++ codeforces/1354/B.cpp
@@ -1,100 +1,33 @@
-// #pragma comment(linker, "/stack:200000000")
-// #pragma GCC optimize("Ofast")
-// #pragma GCC target("avx,avx2,fma")
-// #pragma GCC optimization("unroll-loops")
+#include<bits/stdc++.h>
 
-#include "bits/stdc++.h"
-#define pb push_back
-#define mp make_pair
-#define mt make_tuple
-#define endl '\n'
-#define F first
-#define S second
-#define IO ios::sync_with_stdio(0);cin.tie(0);cout.tie(0)
-#define cf ll testcases;cin>>testcases;while(testcases--)
-#define prn(x) if(x) cout<<"YES"<<endl; else cout<<"NO"<<endl;
-#define all(x) x.begin(),x.end()
-#define rall(x) x.rbegin(),x.rend()
-#define minel(x) (*min_element(all(x)))
-#define maxel(x) (*max_element(all(x)))
-#define cou dbg=1;cout
 using namespace std;
-typedef long long ll;
-typedef long double ld;
-typedef pair<ll,ll> pi;
-typedef vector<ll> vi;
-typedef vector<ld> vd;
-typedef vector<pi> vp;
-ll dbg=0;
-template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p);
-template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
-template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p);
-template<typename A> istream& operator>>(istream& cin, vector<A> &v);
-int dx[] = {-1, -1, -1,  0, 0,  1, 1, 1};
-int dy[] = {-1,  0,  1, -1, 1, -1, 0, 1};
 
-void solve(){
-    string s;
-    cin>>s;
-    vector<pair<ll,ll>> arr;
-    char c=s[0];
-    ll co=1;
-    ll n=s.size();
-    for(ll i=1;i<=n;i++){
-        if(i==n){
-            arr.pb({c-'0',co});
-            continue;
-        }
-        if(s[i]!=c){
-            arr.pb({c-'0',co});
-            co=0;
-            c=s[i];
-        }
-        co++;
-    }
-    // for(auto i:arr){
-    //     cou<<i<<' ';
-    // }
-    // cout<<endl;
-    ll ans=LLONG_MAX;
-    for(ll i=1;i<arr.size()-1;i++){
-        if(arr[i-1].F!=arr[i+1].F){
-            ans=min(ans,arr[i].S+2);
-        }
-    }
-    if(arr.size()<3) ans=LLONG_MAX;
-    if(ans>n) cout<<0<<endl;
-    else cout<<ans<<endl;
-}
+char buf[200043];
 
-int main(){
-    IO;
-    cf
-    solve();
-    return 0;
-}
-
-
-template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { 
-    if(dbg){dbg=0;
-        return cout << "(" << p.F << ", " << p.S << ")"; 
-    }else{
-        return cout<<p.F<<' '<<p.S;
-    }
-        }
-template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
-    if(dbg){dbg=0;
-        cout << "["; for(ll i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
-    }else{
-        for(ll i=0;i<v.size()-1;i++) cout<<v[i]<<' ';
-        return cout<<v[v.size()-1];
-    }
-}
-template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
-   cin >> p.first;
-   return cin >> p.second;
-}
-template<typename A> istream& operator>>(istream& cin, vector<A> &v) {
-   for(ll i=0;i<v.size()-1;i++) cin>>v[i];
-   return cin >> v[v.size()-1];
+int main()
+{
+	int t;
+	scanf("%d", &t);
+	for(int i = 0; i < t; i++)
+	{
+		scanf("%s", buf);
+		string s = buf;
+		int ans = int(1e9);
+		int n = s.size();
+		vector<pair<char, int> > c;
+		for(auto x : s)
+		{
+			if(c.empty() || c.back().first != x)
+				c.push_back(make_pair(x, 1));
+			else
+				c.back().second++;
+		}
+		int m = c.size();
+		for(int i = 1; i < m - 1; i++)
+			if(c[i - 1].first != c[i + 1].first)
+				ans = min(ans, c[i].second + 2);
+		if(ans > n)
+			ans = 0;
+		printf("%d\n", ans);
+	}
 }
